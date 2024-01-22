24f092c93171d5d173417f0c9e343539359667d8
Vasanth <vasanth@Vasanths-MacBook-Air.local>
2024-01-22 20:39:07 +0530

    Main Updated

---
+++ FrontEnd /src/App.jsx
@@ -0,0 +1,10 @@
+import Hello_World from './components/Hello_World';
+
+function App() {
+  return (
+    <>
+    <Hello_World/>
+    </>
+  )
+}
+export default App
---
+++ FrontEnd /src/components/Hello_World.jsx
@@ -0,0 +1,4 @@
+function Hello_World(){
+    return(<p>Hello World!!!</p>);
+}
+export default Hello_World
