57b0d7b3a86dc2ca6edc23b3110754b5ce68a281
Vasa-m <vasanthmv27@gmail.com>
2024-01-31 11:44:07 +0530

    Day1

--- FrontEnd /src/components/Hello_World.jsx
+++
@@ -1,4 +0,0 @@
-function Hello_World(){
-    return(<p>Hello from Vasa</p>);
-}
-export default Hello_World
--- FrontEnd /src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-function About() {
-    return ( 
-        <h1>AboutUs</h1>
-     );
-}
-
-export default About;
